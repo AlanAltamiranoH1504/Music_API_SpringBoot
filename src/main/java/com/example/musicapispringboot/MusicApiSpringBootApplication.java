e9ac8b0cdfb738dfd0e918d0986a1bb945c2f3ac
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2026-04-18 11:06:24 -0600

    initial commit

---
+++ src/main/java/com/example/musicapispringboot/MusicApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicApiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicApiSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/musicapispringboot/MusicApiSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApiSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
